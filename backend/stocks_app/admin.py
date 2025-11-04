bb96a502135e5a15ff64f3cbc3f776cc808f93e3
Witwin12 <kunwin12@gmail.com>
2025-11-04 17:01:41 +0700

    make django admin page

--- backend/stocks_app/admin.py
+++ backend/stocks_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models.employee import Employee 
+from .models.stock_out_transaction import StockOutTransaction
+from .models.tire_lot import TireLot
+from .models.tire_product import TireProduct
 
-# Register your models here.
+admin.site.register(Employee)
+admin.site.register(StockOutTransaction)
+admin.site.register(TireLot)
+admin.site.register(TireProduct)
