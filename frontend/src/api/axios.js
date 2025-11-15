a5d9cc1f2543ecf13f1a0a5602d8e691ead0d15a
Witwin12 <kunwin12@gmail.com>
2025-11-15 11:24:10 +0700

    update axios.js for development and production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: import.meta.env.VITE_API_URL
 })
-
 // เพิ่ม interceptor สำหรับ token อัตโนมัติ
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('authToken')
